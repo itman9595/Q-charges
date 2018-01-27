beeb4f3c93a597aa283b4a82c952ab07f718587e
Muratbek Bauyrzhan <itman9595@yahoo.com>
2018-01-27 12:44:55 +0600

    First Recommit plus

---
+++ Tracker/Tracker macOS/Controller/MainViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MainViewController.swift
+//  Tracker macOS
+//
+//  Created by Muratbek Bauyrzhan on 1/27/18.
+//  Copyright © 2018 Q Charge. All rights reserved.
+//
+
+import Cocoa
+import SceneKit
+
+class MainViewController: NSViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
