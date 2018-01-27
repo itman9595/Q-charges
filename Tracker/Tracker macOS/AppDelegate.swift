3537a38cd736545aaa0d9129dbd94a9205cbfaef
Muratbek Bauyrzhan <itman9595@yahoo.com>
2018-01-27 12:25:29 +0600

    First Commit

---
+++ Tracker/Tracker macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Tracker macOS
+//
+//  Created by Muratbek Bauyrzhan on 1/27/18.
+//  Copyright © 2018 Q Charge. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
